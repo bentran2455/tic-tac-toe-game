ba779a00a2c633dadd2a9aa7fdb7a76cd8898a9e
Ben Tran <letscontactben@gmail.com>
2024-08-23 14:59:49 +0700

    Update game.js

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,15 +6,11 @@ function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [winner, setWinner] = useState(null);
 
-  //Declaring a Winner
   useEffect(() => {
     const newWinner = calculateWinner(squares);
     setWinner(newWinner);
   }, [squares]);
 
-  //function to check if a player has won.
-  //If a player has won, we can display text such as “Winner: X” or “Winner: O”.
-  //Input: squares: given an array of 9 squares:'X', 'O', or null.
   const calculateWinner = (squares) => {
     const lines = [
       [0, 1, 2],
@@ -39,7 +35,6 @@ function Game() {
     return null;
   };
 
-  //Handle player
   const handleClick = (i) => {
     const newSquares = squares.slice();
 
@@ -53,7 +48,6 @@ function Game() {
     setXIsNext((prevState) => !prevState);
   };
 
-  //Restart game
   const handlRestart = () => {
     setSquares(Array(9).fill(null));
     setXIsNext(true);
